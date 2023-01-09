7b2151d23aaf04f264ac10df0423448c485ad4e4
Simon Willison <swillison@gmail.com>
2023-01-09 11:26:32 -0800

    datasette-auth-passwords to allow specific user to execute SQL

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+
+if [ -z "$SIMON_HASH" ]; then
+    echo "Error: SIMON_HASH environment variable is not set - should contain a password hash" >&2
+    exit 1
+fi
+
 datasette publish cloudrun counties.db \
     -m metadata.yml \
+    --plugin-secret datasette-auth-passwords simon_password_hash "$SIMON_HASH" \
     --spatialite \
     --install datasette-leaflet-geojson \
     --install datasette-block-robots \
+    --install datasette-auth-passwords \
     --service us-county-polygons
